2a11f72f33e795c6002ac661c58094e4e35b7bc1
Sekine <tomatotamada@users.noreply.github.com>
2026-02-13 22:44:29 +0900

    make easy field

--- tetrust/src/main.rs
+++ tetrust/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
     //フィールドを描画
-    for y in 0..20 {
-        for x in 0..11 {
-            print!("[]");
+    for y in 0..21 {
+        for x in 0..13 {
+            if x == 0 || x == 12 || y == 20 {
+                print!("[]");
+            }
+            else {
+                print!(" .");
+            }
+
         }
         println!();
     }
