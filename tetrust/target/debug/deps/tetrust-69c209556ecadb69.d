c946d3cb15df886f07bb4d26047e05090b0a7833
Sekine <tomatotamada@users.noreply.github.com>
2026-02-14 00:42:20 +0900

    fix fall speed

---
+++ tetrust/target/debug/deps/tetrust-69c209556ecadb69.d
@@ -0,0 +1,5 @@
+/home/iori/projects/Rust_tetris/tetrust/target/debug/deps/tetrust-69c209556ecadb69.d: src/main.rs
+
+/home/iori/projects/Rust_tetris/tetrust/target/debug/deps/libtetrust-69c209556ecadb69.rmeta: src/main.rs
+
+src/main.rs:
